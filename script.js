fbe8ca098a608764d62f94d10688c64fb01174ce
Chukwuebuka Alaegbu <chukwuebukaalaegbu@gmail.com>
2023-12-30 18:11:57 +0100

    creating objects for each song

--- script.js
+++ script.js
@@ -7,4 +7,13 @@ const previousButton = document.getElementById("previous")
 const shuffleButton = document.getElementById("shuffle")
 
 // This array will hold all songs
-const allSongs = []
+//each song will be an object
+const allSongs = [
+    {
+        id: 0,
+        title: "Scratching The Surface",
+        artist: "Quincy Larson",
+        duration: "4:25",
+        src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/scratching-the-surface.mp3",
+    }
+]
