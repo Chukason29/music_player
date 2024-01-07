660beced733f5114271f91e7748fee243fa72a11
Chukwuebuka Alaegbu <chukwuebukaalaegbu@gmail.com>
2024-01-07 14:36:15 +0100

    added id attributr to reset

--- script.js
+++ script.js
@@ -170,6 +170,8 @@ const playNextSong = () => {
     if (userData.songs.length === 0) {
         const resetButton = document.createElement("button")
         const resetText = document.createTextNode("Reset Playlist")
+        resetButton.id = "reset"
+        resetButton.ariaLabel = "Reset playlist"
     }
 
   };
